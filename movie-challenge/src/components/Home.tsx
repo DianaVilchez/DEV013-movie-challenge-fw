28463ad4acf1c9bc37150dc40f744bec700d8a6a
Diana <educ.dana@gmail.com>
2024-07-24 13:01:40 -0500

    configuración inicial del componente Home

---
+++ movie-challenge/src/components/Home.tsx
@@ -0,0 +1,16 @@
+import MovieList from "./MovieList";
+
+function Home () {
+    return(
+//   <main className = 'Home'>
+//     <article className = 'Banner'>
+//     </article>
+//     <section className = 'Home-Body'>
+        <div>
+          <MovieList movies={[]}/>
+        </div>
+//     </section>
+//   </main>
+  )
+}
+export default Home
